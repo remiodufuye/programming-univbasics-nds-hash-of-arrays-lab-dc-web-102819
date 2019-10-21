d170321720e245336bcae3dca725296eaa58afc7
remi.odufuye <remi.odufuye@gmail.com>
2019-10-21 16:09:12 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -10,6 +10,6 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   
-  show[:temperature] << 76 #=> [75, 80, 72, 76]
+  show[:third_earthers] << 76 
   
 end
